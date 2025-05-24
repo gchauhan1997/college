984bdabbb5a2816de15af2042bbcacbefc1ef745
Gaurav <gdhikka@mail.com>
2025-05-24 21:43:13 +0530

    Add campus information section, implement testimonial slider, and include new images for enhanced layout and design

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,31 @@ counterRef.once('value').then(snapshot => {
 counterRef.on('value', snapshot => {
   document.getElementById('visitor-count').innerText = snapshot.val();
 });
+// slider
+$(document).ready(function () {
+  $('.testimonial-slider-info-2').slick({
+    dots: true,
+    infinite: true,
+    arrows:false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
